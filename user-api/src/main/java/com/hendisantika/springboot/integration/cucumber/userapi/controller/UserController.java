104032b2af5ba23f745dcd1a6db6ff4d889b1acb
hendisantika <hendisantika@yahoo.co.id>
2019-07-11 06:51:58 +0700

    #9 - Add getUsers method in UserController in user-api module

--- user-api/src/main/java/com/hendisantika/springboot/integration/cucumber/userapi/controller/UserController.java
+++ user-api/src/main/java/com/hendisantika/springboot/integration/cucumber/userapi/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springboot.integration.cucumber.userapi.controller;
 
+import com.hendisantika.springboot.integration.cucumber.userapi.domain.User;
 import com.hendisantika.springboot.integration.cucumber.userapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +27,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping(value = {"/", "/users"})
+    public ResponseEntity<?> getUsers() {
+        List<User> users = userService.getUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 }
