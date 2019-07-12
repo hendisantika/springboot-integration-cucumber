8945955213c5ab733b2e78344218d29a4fa8487b
hendisantika <hendisantika@yahoo.co.id>
2019-07-13 05:36:38 +0700

    #29 - Add steps/GivenTest in user-api-testing module

---
+++ user-api-testing/src/test/java/com/hendisantika/springbootintegrationcucumber/userapitesting/glue/steps/GivenTest.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootintegrationcucumber.userapitesting.glue.steps;
+
+import cucumber.api.java.en.Given;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-integration-cucumber
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-13
+ * Time: 05:36
+ */
+public class GivenTest {
+
+    @Given("^Test Given$")
+    public void test_Given() throws Throwable {
+        assertTrue(true);
+    }
+}
